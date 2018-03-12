827e3f44654c056d2d7affb8464b004931816308
avnerrhh <avnerrhh@gmail.com>
2018-03-12 11:10:46 +0200

    add a new function edge_set_to_graph

--- Ramsey.py
+++ Ramsey.py
@@ -1,9 +1,6 @@
 import copy
 import sys
 
-import itertools
-import scipy.special
-
 
 def is_bit_set(num, bit):
     return num & (1 << bit) > 0
@@ -15,9 +12,6 @@ def subsets(s):
         subset = [s[bit] for bit in range(len(s)) if is_bit_set(i, bit)]
         sets.append(subset)
     return sets
-
-
-solution = []
 
 
 def get_subset(super_set, k, idx, current):
@@ -79,10 +73,11 @@ def write_to_graph_to_file(graph):
                 output.write(",\n")
                 output.write("     [")
             if 1 in graph[key]:
+                print "hello"
                 output.write("0")
             else:
                 output.write("1")
-            for i in range(2, len(graph.keys())):
+            for i in range(2, len(graph.keys())+1):
                 output.write(",")
                 if i in graph[key]:
                     output.write("0")
@@ -93,11 +88,50 @@ def write_to_graph_to_file(graph):
         output.write("\n")
         output.write("]).")
 
+
+def edge_set_to_graph(edge_set, graphSize):
+    ans = {}
+    for i in range (1,graphSize+1):
+        ans[i] = []
+
+    for edge in edge_set:
+        ans[edge[0]].append(edge[1])
+    return ans
+
+#get a subset from all the edges by the index in a binary manner
+# e.g index = 64 -> only the edge at the 6th place will return, index = 127 -> 1 - 6 edges will return
+def get_subset_by_number(all_edges, index):
+    ans = []
+    for i in range(0,len(all_edges)):
+        if (1 & index >> i):
+            ans.append(all_edges[i])
+    return ans
+
+
+
 if __name__ == "__main__":
     color1 = int(sys.argv[1])
     color2 = int(sys.argv[2])
     graphSize = int(sys.argv[3])
-    graph = {}
-print check_graph({1: [3, 4, 5], 2: [4, 5, 6], 3: [1, 5, 6], 4: [1, 2, 6], 5: [1, 2 ,3],6: [2, 3, 4]}, 3, 3)
-print check_graph({1: [2,6], 2: [1, 3], 3: [2, 4], 4: [3, 5], 5: [4, 6],6: [1, 5]}, 3, 3)
-write_to_graph_to_file({1: [2,6], 2: [1, 3], 3: [2, 4], 4: [3, 5], 5: [4, 6],6: [1, 5]})
+    vertex = []
+    for i in range(1, graphSize + 1):
+        vertex.append(i)
+    global solution
+    solution = []
+    get_subset(vertex, 2, 0, [])
+    # print all_edges
+    # answer_flag = True
+    # for edge_set in all_edges:
+    #     graph = edge_set_to_graph(edge_set, graphSize)
+    #     print graph
+    #     if not check_graph(graph, color1, color2):
+    #         write_to_graph_to_file(graph)
+    #         answer_flag = False
+    #         break
+    # if answer_flag:
+    #     with open('OUTPUT.txt', 'w') as output:
+    #         output.write("NO SOLUTION")
+
+#print check_graph({1: [3, 4, 5], 2: [4, 5, 6], 3: [1, 5, 6], 4: [1, 2, 6], 5: [1, 2 ,3],6: [2, 3, 4]}, 3, 3)
+#print check_graph({1: [2,6], 2: [1, 3], 3: [2, 4], 4: [3, 5], 5: [4, 6], 6: [1, 5]}, 3, 3)
+#write_to_graph_to_file({1: [2,6], 2: [1, 3], 3: [2, 4], 4: [3, 5], 5: [4, 6],6: [1, 5]})
